fe3ec412c0295dc8fce0bb80b9d86f8f178fac3e
mdnihal5 <mdnihal.private1@gmail.com>
2024-06-12 10:40:48 +0530

    portfolio setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
-import Navbar from './components/Navbar'
-import Socials from "./components/Socials"
+import Navbar from "./components/Navbar";
+import Socials from "./components/Socials";
 import { Toaster } from "react-hot-toast";
 
 const items = [
-
-    { label: 'About', href: '/about' },
-
-    { label: 'Project', href: '/work' },
-
-    { label: 'Skills', href: '/skills' },
-
-  ];
+    { label: "About", href: "/about" },
+    { label: "Project", href: "/work" },
+    { label: "Skills", href: "/skills" },
+];
 
 const App = () => (
-  <>
-  <Navbar items={items}/>
-  <Socials/>
-  <Toaster position="top-center" />
-  </>
+    <>
+        <Navbar items={items} />
+        <Socials />
+        <Toaster position="top-center" />
+    </>
 );
 export default App;

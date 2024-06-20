ec799661bf648ae112e31ea9a2dbfd0855830482
Md Nihal <111081295+mdnihal5@users.noreply.github.com>
2024-06-20 14:49:07 +0530

    Update Resume.tsx

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 const Resume = () => {
-    const downloadUrl = "https://drive.google.com/uc?export=download&id=141_xyNpY6KRFUxHP6Nt5AXCR_Gdf2Z1g";
+    const downloadUrl = "https://drive.google.com/uc?export=download&id=14jhAR6vVqPyRifPs_1QF2pgqCnfn51pr";
 
     return (
         <Link to={downloadUrl} target="_blank" rel="noopener noreferrer" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
